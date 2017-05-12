923fc72ea921c844c40d459f8362766a08001f74
Chris Keller <christopherlawrencekeller@gmail.com>
2017-05-12 12:17:56 -0700

    updates and first pass fixes

--- js/DataConverter.js
+++ js/DataConverter.js
@@ -25,7 +25,7 @@ function DataConverter(nodeId) {
     this.inputText              = "";
     this.outputText             = "";
     this.newLine                = "\n";
-    this.indent                 = "  ";
+    this.indent                 = "    ";
     this.commentLine            = "//";
     this.commentLineEnd         = "";
     this.tableName              = "MrDataConverter"
@@ -86,6 +86,13 @@ DataConverter.prototype.convert = function(){
         }
         CSVParser.resetLog();
         var parseOutput = CSVParser.parse(this.inputText, this.headersProvided, this.delimiter, this.downcaseHeaders, this.upcaseHeaders);
+
+
+
+        console.log(parseOutput);
+
+
+
         var dataGrid = parseOutput.dataGrid;
         var headerNames = parseOutput.headerNames;
         var headerTypes = parseOutput.headerTypes;
@@ -93,20 +100,18 @@ DataConverter.prototype.convert = function(){
         this.outputText = DataGridRenderer[this.outputDataType](dataGrid, headerNames, headerTypes, this.indent, this.newLine, this.id, this.classname);
         var styles = "";
         styles += "<style type=\"text/css\">\n";
-        styles += "\ttable {text-align: left; border-collapse: collapse; margin:0 0 14px 0; width: 100%;}\n";
+        styles += "\ttable {text-align: left; max-width: 650px; width: 100%; background-color: transparent; border-collapse: collapse; border-spacing: 0; border-color: #CCC; margin: 50px auto; font-family: 'Benton Gothic Regular', Arial, sans-serif; font-size: 15px;}\n";
         styles += "\ttable th {text-transform: capitalize;}\n";
         styles += "\ttable td mark {display: none;}\n";
         styles += "\ttable tr td:first-child mark {display: none;}\n";
-        // styles += "\ttbody tr:nth-child(odd) td, tbody tr.odd td {background-color: #eee;}\n";
         styles += "\t@media screen and (max-width: 550px){\n";
         styles += "\t\ttable {border-top: 3px solid #4591B8; width: 100%;}\n";
         styles += "\t\ttable thead {display: none;}\n";
         styles += "\t\ttable tbody, table tr, {width: 100%}\n";
-        // styles += "\t\ttable tbody tr:nth-child(odd) td, table tbody tr.odd td {background-color: #eee;}\n";
         styles += "\t\ttable td {display: block; padding: 10px 5px 10px 5px;}\n";
-        styles += "\t\ttable td mark {display: inline; background: none; color: #D17333; }\n";
-        styles += "\t\ttable td mark:after {content: \": \"; margin-right: 50%; }\n";
-        styles += "\t\ttable tr td:first-child {background: none; font-weight: 800; font-size: 1.0em; border-bottom-color: #808080; padding-top: 15px;}\n";
+        styles += "\t\ttable td mark {display: inline; background: none; color: #282828; }\n";
+        styles += "\t\ttable td mark:after {content: \": \"; }\n";
+        styles += "\t\ttable tr td:first-child {background: none; font-weight: 600; font-size: 1.1em; border-bottom-color: #808080; padding-top: 15px;}\n";
         styles += "\t\ttable tr td:nth-child(2) {padding-top: 10px;}\n";
         styles += "\t\ttable tr td:last-child {padding-bottom: 20px;}\n";
         styles += "\t}\n";
@@ -134,18 +139,4 @@ DataConverter.prototype.convert = function(){
     };
     $("#select-sort").html(selectOptions);
     this.sort();
-};
-
-DataConverter.prototype.sort = function(){
-    //TODO sorting breaks even/odd classes
-    //TODO is this the correct way to do checkboxes?
-    if (this.sortable === "on") {
-        this.sortOptions = {
-            sortList: [
-                [this.sortColumn, this.sortOrder]
-            ]
-        };
-        $("#table-1").tablesorter(this.sortOptions);
-    }
-    //TODO else refresh content from input (overrides preview sort
 };
--- js/DataGridRenderer.js
+++ js/DataGridRenderer.js
@@ -10,50 +10,51 @@ var DataGridRenderer = {
     // HTML Table
     //---------------------------------------
 
-    html: function(dataGrid, headerNames, headerTypes, indent, newLine, id, classname) {
+    html: function(dataGrid, headerNames, headerTypes, indent, newLine, id, classname){
         //inits...
         var commentLine = "<!--";
         var commentLineEnd = "-->";
         var outputText = "";
         var numRows = dataGrid.length;
         var numColumns = headerNames.length;
-        //begin render loop
         outputText += "<table id=\"" + id + "\" class=\"" + id + "\">" + newLine;
         outputText += indent + "<thead>" + newLine;
         outputText += indent + indent + "<tr>" + newLine;
-        for (var j = 0; j < numColumns; j++) {
-            outputText += indent + indent + indent + '<th class="' + headerNames[j] + '-cell">';
+        for (var j=0; j<numColumns; j++){
+            outputText += indent + indent + indent;
+            outputText += '<th class="' + headerTypes[j] + " " + headerNames[j] + '_cell">';
             outputText += headerNames[j];
             outputText += '</th>' + newLine;
         };
         outputText += indent + indent + "</tr>" + newLine;
         outputText += indent + "</thead>" + newLine;
         outputText += indent + "<tbody>" + newLine;
-        for (var i = 0; i < numRows; i++) {
+        for (var i=0; i<numRows; i++){
             var row = dataGrid[i];
             var classes = [];
-            if (i === numRows-1) {
+            if (i === numRows-1){
                 classes.push("lastRow");
             } else if (i === 0){
                 classes.push("firstRow");
             }
-            if (i % 2) {
+            if (i % 2){
                 classes.push("even");
             } else {
                 classes.push("odd");
             }
             var rowClassName = " class=\"";
             var classesLength = classes.length;
-            for (j in classes) {
+            for (j in classes){
                 rowClassName += classes[j];
-                if (j == 0 && classesLength > 1) {
+                if (j == 0 && classesLength > 1){
                     rowClassName += " ";
                 }
             }
             rowClassName += "\"";
             outputText += indent + indent + "<tr" + rowClassName + ">" + newLine;
-            for (var j = 0; j < numColumns; j++) {
-                outputText += indent + indent + indent + '<td class="' + headerNames[j] + '-cell">';
+            for (var j=0; j<numColumns; j++){
+                outputText += indent + indent + indent;
+                outputText += '<td class="' + headerTypes[j] + " " + headerNames[j] + '_cell">';
                 outputText += "<mark>" + headerNames[j] + "</mark>";
                 outputText += row[j];
                 outputText += '</td>' + newLine;
